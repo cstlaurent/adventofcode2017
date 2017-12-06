a78e8a9d98ea46c87914d826324b2f1b71ce5030
Carl St-Laurent <cstlaurent@genetec.com>
2017-12-05 21:04:13 -0500

    day5 p2

--- day5/main.go
+++ day5/main.go
@@ -12,11 +12,11 @@ func main() {
 	s := string(dat[:])
 
 	lines := strings.Split(s, "\r\n")
-	fmt.Printf("Day 5 part 1: %d steps\n", calcJumps(lines))
+	fmt.Printf("Day 5 part 1: %d steps\n", calcJumps(lines, false))
+	fmt.Printf("Day 5 part 2: %d steps\n", calcJumps(lines, true))
 }
 
-func calcJumps(lines []string) int {
-	fmt.Println(len(lines))
+func calcJumps(lines []string, day2 bool) int {
 	jumps := make([]int, len(lines))
 	for i, val := range lines {
 		integ, err := strconv.Atoi(val)
@@ -25,19 +25,17 @@ func calcJumps(lines []string) int {
 		}
 		jumps[i] = integ
 	}
-	currentVal := 0
-	oldVal := 0
-	jumpsTaken := 0
-	for {
-		// fmt.Println(currentVal)
-		oldVal = currentVal
-		currentVal = currentVal + jumps[currentVal]
-
-		jumpsTaken++
-		if currentVal < 0 || currentVal >= len(lines) {
-			break
+	current := 0
+	steps := 0
+	for current < len(jumps) && current >= 0 {
+		steps++
+		offset := jumps[current]
+		if day2 && offset >= 3 {
+			jumps[current]--
+		} else {
+			jumps[current]++
 		}
-		jumps[oldVal]++
+		current += offset
 	}
-	return jumpsTaken
+	return steps
 }
